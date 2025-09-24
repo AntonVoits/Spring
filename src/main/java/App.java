3b296f55687f87583b321adb24e7f92b2ee14b32
Anton Voitsekhovskii <antonvoits@mail.ru>
2025-09-24 15:30:22 +0300

    Added Cat class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,19 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class App {
+public class   App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        //System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("For Helloworld beans pair: " + (bean == bean2));
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("For Cat beans pair: " + (catBean == catBean2));
     }
 }
