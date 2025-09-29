4331225c92d8b86078c3e986f706586184fb1e4f
Anton Voitsekhovskii <antonvoits@mail.ru>
2025-09-29 16:18:17 +0300

    Added class field, constructor and set Method of connection.

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -9,7 +9,9 @@ public class Needle7 {
     private final Deth8 deth;
 
     @Autowired
-    public Needle7(Deth8 deth) {this.deth = deth;}
+    public Needle7(Deth8 deth) {
+        this.deth = deth;
+    }
 
     @Override
     public String toString() {
